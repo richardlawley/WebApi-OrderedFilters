5f5b6704e85aeffa3ac5c6aa39b547f0f69db33a
Richard Lawley <richard@richardlawley.com>
2013-03-08 15:29:42 +0000

    Fixed nuget build

--- src/RichardLawley.WebApi.OrderedFilters/Properties/AssemblyInfo.cs
+++ src/RichardLawley.WebApi.OrderedFilters/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RichardLawley.WebApi.OrderedFilters")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Ordered Filters for Web API")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Richard Lawley")]
 [assembly: AssemblyProduct("RichardLawley.WebApi.OrderedFilters")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © Richard Lawley 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
